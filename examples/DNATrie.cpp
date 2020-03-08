4f6cd017085ed3342272aba2191e6ce499ddb07e
Joshua Weinstein <jweinstein@splunk.com>
2020-03-07 22:29:21 -0800

    trie find and insert

--- examples/DNATrie.cpp
+++ examples/DNATrie.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <cstdio>
 #include <cstring>
+#include <cassert>
 #include "TestObject.h"
 
 static TestObject gTester;
@@ -48,12 +49,28 @@ void DNA::fromCStr(DNA::Base* dest, size_t dest_size, const char* src)
     }
 }
 
+struct DNAChunk {
+    DNAChunk(const char* string): dsize(strlen(string)),
+                                  dna(new DNA::Base[dsize])
+   {
+       DNA::fromCStr(dna, dsize, string);
+   }
+    ~DNAChunk()
+    {
+        delete[] dna;
+        dsize = 0;
+    }
+    
+    size_t dsize;
+    DNA::Base* dna;
+};
+
 struct DNANode {
 				
-	DNANode(DNA::Base* d1 = nullptr,
-	        DNA::Base* d2 = nullptr,
-			DNA::Base* d3 = nullptr,
-			DNA::Base* d4 = nullptr): count(0),
+	DNANode(DNANode* d1 = nullptr,
+	        DNANode* d2 = nullptr,
+			DNANode* d3 = nullptr,
+			DNANode* d4 = nullptr): count(-1),
 			childNodes{d1, d2, d3, d4}
 				{}
 				
@@ -69,12 +86,44 @@ struct DNANode {
 			   childNodes[2] == nullptr &&
 			   childNodes[3] == nullptr;
 	}
+    
+    void insert(const DNA::Base* dna, size_t size);
+    long find(const DNA::Base* dna, size_t size);
 	
-	unsigned long count;
+	long count;
     DNANode* childNodes[4];
 };
 
+void DNANode::insert(const DNA::Base* dna, size_t size)
+{
+    assert(size);
+    if (size == 1) {
+        count = count == -1 ? 0 : count + 1;
+        return;
+    }
+    DNANode** nextNode = &childNodes[*dna];
+    if (*nextNode == nullptr) {
+        *nextNode = new DNANode();
+    }
+    (*nextNode)->insert(dna + 1, size - 1);
+}
+
+long DNANode::find(const DNA::Base* dna, size_t size)
+{
+    assert(size);
+    if (size == 1)
+        return count;
+    DNANode* nextNode = childNodes[*dna];
+    if (nextNode == nullptr)
+        return -1;
+    else
+        return nextNode->find(dna + 1, size - 1);
+}
+
 int main(int argc, char const* argv[])
 {
+    gTester.clear();
+    
+    gTester.finish();
     return 0;
 }
